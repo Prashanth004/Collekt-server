14d3eb8155357b91a4118e17488938e96ef53e2d
bookmane <bookmanelabs@gmail.com>
2019-10-03 08:42:58 +0000

    egfefg

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//app.js
 const express = require('express');
 var socket = require('socket.io');
 const passport = require('passport');
@@ -6,12 +5,12 @@ const cookieSession = require('cookie-session');
 const passportSetup = require('./config/passport-setup');
 const bodyParser = require('body-parser');
 const product = require('./routes/Card.route'); // Imports routes for the products
-const lists= require('./routes/List.router')
+const lists = require('./routes/List.router')
 const user = require('./routes/user.router')
 const share = require('./routes/shared.route')
 const admin = require('./routes/admin.route')
 const authRoutes = require('./routes/auth-routes')
-const basic = require('./routes/basic.routes')
+const basic = require('./routes/basic-routes')
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
@@ -23,8 +22,8 @@ app.use(cookieSession({
   keys: [keys.session.cookieKey]
 }));
 app.use(passport.initialize());
-app.use(passport.session());
 
+app.use(passport.session());
 
 mongoose.connect('mongodb://localhost/myappdatabase');
 
@@ -44,39 +43,40 @@ app.use(express.static(publicDir));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(bodyParser.json());
 
+app.use(bodyParser.json());
+app.use('/', basic );
 app.use('/auth', authRoutes);
 app.use('/product', product);
 app.use('/list', lists);
 app.use('/user', user);
 app.use('/admin', admin);
 app.use('/share', share);
-app.use('/', basic );
+
 // app.get('/facen')
-app.use( [
+app.use([
   handleError
-  ] );
-function handleError(err,req,res,next){
+]);
+function handleError(err, req, res, next) {
   var output = {
-      error: {
-          name: err.name,
-          message: err.message,
-          text: err.toString()
-      }
+    error: {
+      name: err.name,
+      message: err.message,
+      text: err.toString()
+    }
   };
 
   var statusCode = err.status || 500;
   res.status(statusCode).json(output);
 }
-app.get('*', function(req, res, next) {
+app.get('*', function (req, res, next) {
   let err = new Error('Page Not Found');
   err.statusCode = 404;
   next(err);
 });
 
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   if (!err.statusCode) err.statusCode = 500; // If err has no specified error code, set error code to 'Internal Server Error (500)'
   res.status(err.statusCode).send(err.message); // All HTTP requests must have a response, so let's send back an error with its status code and message
 });
@@ -86,53 +86,53 @@ let port = 1234;
 var server = app.listen(port, () => {
 });
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 // Socket setup & pass server
 io = socket(server);
 io.on('connection', (socket) => {
 
-    console.log('made socket connection', socket.id);
+  console.log('made socket connection', socket.id);
 
-    // Handle chat event
-    socket.on('closeiframe',function(data){
-      console.log(data);
-      io.sockets.emit('closeiframe', data)
-    })
-    socket.on('logout', function(data){
-        console.log(data);
-        io.sockets.emit('logout', data);
-        dataframe={
-          close:1,
-          refresh:1
-        }
-        io.sockets.emit('closeiframe', dataframe)
-    });
- 
- 
-    socket.on('list_created', function (data) {
-        console.log(data);
-        console.log(data.data.data)
-        io.sockets.emit('list_created', data.data.data);
-  
-});
-socket.on('deleteLst', function (data) {
-  console.log(data);
-  
-  io.sockets.emit('deleteLst', data.ID);
+  // Handle chat event
+  socket.on('closeiframe', function (data) {
+    console.log(data);
+    io.sockets.emit('closeiframe', data)
+  })
+  socket.on('logout', function (data) {
+    console.log(data);
+    io.sockets.emit('logout', data);
+    dataframe = {
+      close: 1,
+      refresh: 1
+    }
+    io.sockets.emit('closeiframe', dataframe)
+  });
+
+
+  socket.on('list_created', function (data) {
+    console.log(data);
+    console.log(data.data.data)
+    io.sockets.emit('list_created', data.data.data);
+
+  });
+  socket.on('deleteLst', function (data) {
+    console.log(data);
+
+    io.sockets.emit('deleteLst', data.ID);
+
+  });
+  socket.on('cardAdded', function (data) {
+    console.log("crad", data)
+    io.sockets.emit('cardAdded', data);
+  })
+  socket.on('delete', function (data) {
+
+    io.sockets.emit('delete', data.ID);
+
+  });
 
 });
-socket.on('cardAdded',function(data){
-  console.log("crad",data)
-  io.sockets.emit('cardAdded', data);
-})
-socket.on('delete', function (data) {
- 
-  io.sockets.emit('delete', data.ID);
-
-});
-
-});
-exports.io =io;
+exports.io = io;
 
 
